30c0ade07fb6843beb19d0a86a99b4986b543caf
Ben Kaan <benkaan001@gmail.com>
2021-10-27 15:07:02 -0500

    setup/test connection to the database

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 // import EX to create a connection to the Express.js server to host the application
-
 const express = require('express');
 
 // add the PORT designation and the app expression 
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // add the Express.js middleware
-
 app.use(express. urlencoded({ extended: false}));
 app.use(express.json());
+
+// import mysql2 to connect to the MySQL database
+const mysql = require('mysql2');
+
+
+// connect the applicaiton to the database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'atakaan1',
+    database:'mySQL_team'
+},
+console.log ('Connected to the mySQL_team database.')
+);
+
+
+
+
 
 //test the route
 
@@ -18,6 +33,12 @@ app.get('/', (req,res) => {
     res.json({
         message: 'I am working!!'
     });
+});
+
+// add a route that handles requests that are not supported by the app
+
+app.use((req,res) => {
+    res.status(404).end();
 });
 
 // add the function that will start the Express.js server on port 3001
