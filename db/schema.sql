4ec6464a0c9ecca3f29d56f9bb2ac1099bc134f6
Ben Kaan <benkaan001@gmail.com>
2021-10-27 14:49:14 -0500

    update data

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
+DROP DATABASE IF EXISTS mySQL_team;
+
+CREATE DATABASE mySQL_team;
+
+USE mySQL_team;
+
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
@@ -6,18 +12,18 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(6,2),
+    salary DECIMAL,
     department_id INTEGER NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
     FOREIGN KEY (role_id) REFERENCES role(id),
-    manager_id INTEGER NULL,
+    manager_id INTEGER,
     FOREIGN KEY (manager_id) REFERENCES role(id)
 );
